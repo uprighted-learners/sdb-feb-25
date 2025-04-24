b51e660f7f57cc663b9f97911f1ebe0929bcf123
Paul Niemczyk <mrpndev@gmail.com>
2025-04-23 21:00:39 -0400

    Blueprint for todo

---
+++ controllers/todos.js
@@ -0,0 +1,29 @@
+const router = require("express").Router()
+
+// get all todo's
+// get one todo by searching for its name
+// add a todo
+// update a todo
+// delete a todo
+
+router.get("/:name", (req, res) => {
+
+})
+router.get("/something", (req, res) => {
+    
+})
+
+router.post("/add", (req, res) => {
+
+})
+
+router.put("/:id", (req, res) => {
+
+})
+
+router.delete("/:id", (req, res) => {
+
+})
+
+
+module.exports = router
